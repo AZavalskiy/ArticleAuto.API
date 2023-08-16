c5e38fd4efb836e64ca8b41fe71c97981a445748
Andrii Zavalskyi <azavalskyi@gmail.com>
2023-08-16 05:49:42 +0300

    Created an UserName field instead of Name/Surname, changed PartitionKey value, delated an extra RepeatPassword field from entity User

--- TFAuto.DAL/Models/User.cs
+++ TFAuto.DAL/Models/User.cs
@@ -2,16 +2,11 @@
 using Microsoft.Azure.CosmosRepository;
 
 namespace TFAuto.DAL;
-
-[PartitionKeyPath("/name")]
-public class User : FullItem
+[PartitionKeyPath("/partitionKey")]
+public class User : BaseEntity
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public virtual string Name { get; set; }
-    public string Surname { get; set; }
+    public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string RepeatPassword { get; set; }
-    protected override string GetPartitionKeyValue() => Name;
-    
+    public override string PartitionKey { get; set; } = nameof(User);
 }
