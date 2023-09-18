e55bc5f8a98c59c21f61af2ad751fd16e2dd7865
Andrii Zavalskyi <azavalskyi@gmail.com>
2023-09-18 09:33:46 +0300

    Added field roleId to registration and login responses

--- TFAuto.Domain/Services/UserRegistration/Models/Response/UserRegistrationResponse.cs
+++ TFAuto.Domain/Services/UserRegistration/Models/Response/UserRegistrationResponse.cs
@@ -3,6 +3,10 @@
 public class UserRegistrationResponse
 {
     public string Id { get; set; }
+
     public string UserName { get; set; }
+
     public string Email { get; set; }
+
+    public string RoleId { get; set; }
 }
