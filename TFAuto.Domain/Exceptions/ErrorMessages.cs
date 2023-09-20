ed72f39c38c36aacd4532040906d0da4af628ea7
Roman Prybluda <romanodua@gmail.com>
2023-09-21 00:38:50 +0300

    added user info

--- TFAuto.Domain/Exceptions/ErrorMessages.cs
+++ TFAuto.Domain/Exceptions/ErrorMessages.cs
@@ -8,6 +8,7 @@ public class ErrorMessages
     public const string USER_VALID_REPEAT_PASSWORD = "Passwords do not match!";
     public const string USER_EXISTS_BY_EMAIL = "An user with the same email already exists!";
     public const string USER_INVALID_INFO = "Personal information isn't valid, please fill the application one more time";
+    public const string USER_NOT_FOUND = "User not found";
 
     public const string ROLES_NOT_FOUND = "Roles not found.";
     public const string ROLE_NOT_FOUND = "Role not found.";
@@ -35,4 +36,12 @@ public class ErrorMessages
     public const string ARTICLE_USER_WHO_UPDATED_NOT_FOUND = "There is no information about the author who made adjustments to the article";
 
     public const string PAGE_NOT_EXISTS = "Page doesn't exist";
+
+    public const string COMMENT_NOT_FOUND = "Comment not found";
+    public const string COMMENTS_NOT_FOUND = "Comments not found";
+    public const string COMMENT_AUTHOR_NOT_FOUND = "Author of comment not found";
+    public const string USER_IS_NOT_COMMENT_AUTHOR = "User is not author of comment";
+    public const string USER_IS_NOT_ARTICLE_AUTHOR = "User is not author of artical";
+    public const string LIKE_AUTHOR_NOT_FOUND = "Author of like not found";
+    public const string COMMENT_PAGE_NOT_EXISTS = "Comments doesn't exist";
 }
---
+++ TFAuto/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+using TFAuto.Domain.Services.UserInfo;
+using TFAuto.Domain.Services.UserInfo.DTO;
+using TFAuto.Domain.Services.UserUpdate;
+using TFAuto.Domain.Services.UserUpdate.DTO;
+
+namespace TFAuto.WebApp.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    //[Authorize]
+
+    public class UserController : ControllerBase
+    {
+        private readonly IUserInfoService _userInfoService;
+        private readonly IUserUpdateInfoService _userUpdateInfoService;
+
+        public UserController(IUserInfoService userInfoService, IUserUpdateInfoService userUpdateInfoService)
+        {
+            _userInfoService = userInfoService;
+            _userUpdateInfoService = userUpdateInfoService;
+        }
+
+        [HttpGet("{userId:Guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(InfoUserResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async ValueTask<ActionResult<InfoUserResponse>> GetUserInfo([Required] Guid userId)
+        {
+            var user = await _userInfoService.GetUserInfo(userId);
+            return Ok(user);
+        }
+
+        [HttpPut("{userId:Guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(UpdateUserInfoResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async ValueTask<ActionResult<UpdateUserInfoResponse>> UpdateUserInfo([Required] Guid userId, [FromQuery] UserUpdateInfoRequest updateInfo)
+        {
+            var user = await _userUpdateInfoService.UpdateUserInfo(userId, updateInfo);
+            return Ok(user);
+        }
+    }
+}
