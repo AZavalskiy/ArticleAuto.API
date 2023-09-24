8003095c8d954814c6ab3b072bba9b2e60f8e1d3
Roman Prybluda <romanodua@gmail.com>
2023-09-24 18:17:01 +0300

    refact: add pass-controller end point to user controller

--- TFAuto/Controllers/UserController.cs
+++ TFAuto/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using Swashbuckle.AspNetCore.Annotations;
 using System.ComponentModel.DataAnnotations;
 using TFAuto.Domain.Services.UserInfo;
 using TFAuto.Domain.Services.UserInfo.DTO;
+using TFAuto.Domain.Services.UserPassword;
+using TFAuto.Domain.Services.UserPassword.DTO;
 using TFAuto.Domain.Services.UserUpdate;
 using TFAuto.Domain.Services.UserUpdate.DTO;
 
@@ -17,11 +19,16 @@ namespace TFAuto.WebApp.Controllers
     {
         private readonly IUserInfoService _userInfoService;
         private readonly IUserUpdateInfoService _userUpdateInfoService;
+        private readonly IUserPasswordService _userPasswordService;
 
-        public UserController(IUserInfoService userInfoService, IUserUpdateInfoService userUpdateInfoService)
+        public UserController(
+            IUserInfoService userInfoService,
+            IUserUpdateInfoService userUpdateInfoService,
+            IUserPasswordService userPasswordService)
         {
             _userInfoService = userInfoService;
             _userUpdateInfoService = userUpdateInfoService;
+            _userPasswordService = userPasswordService;
         }
 
         [HttpGet("{userId:Guid}")]
@@ -42,6 +49,29 @@ namespace TFAuto.WebApp.Controllers
         {
             var user = await _userUpdateInfoService.UpdateUserInfo(userId, updateInfo);
             return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("forgot-password")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(ForgotPasswordResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async ValueTask<ActionResult<ForgotPasswordResponse>> ForgotPassword(ForgotPasswordRequest request)
+        {
+            string callingUrl = Request.GetTypedHeaders().Referer?.AbsoluteUri;
+            var response = await _userPasswordService.ForgotPasswordAsync(request, callingUrl);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("reset-password")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(ResetPasswordResponse))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public async ValueTask<ActionResult<ResetPasswordResponse>> ResetPassword(ResetPasswordRequest request)
+        {
+            var response = await _userPasswordService.ResetPasswordAsync(request);
+            return Ok(response);
         }
     }
 }
